ddf61fdc3fb27ab1eeba9a94f24c4cff47c7c1f1
safiabdulhadi1994 <safiabdulhadi1994@gmail.com>
2022-12-10 21:28:41 +0100

    array_methods

---
+++ array_methods/script.js
@@ -0,0 +1,17 @@
+// let arr = [1, 'harry', 18, 'safi'];
+// console.log(arr);
+
+// arr[2] = 19;
+// console.log(arr);
+// delete arr[2];
+
+// console.log(arr);
+
+
+
+let arr =["sanjay", "Aman","Rehman", "karan"];
+
+console.log(arr);
+
+arr.sort();
+console.log(arr);
